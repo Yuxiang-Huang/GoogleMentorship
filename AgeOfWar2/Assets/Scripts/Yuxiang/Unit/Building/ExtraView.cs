8215da7fa0e7c64b15b54a27af30efab83452e44
Yuxiang Huang <yhuang30@stuy.edu>
2023-03-18 23:28:32 -0400

    damage part of extra view done

--- AgeOfWar2/Assets/Scripts/Yuxiang/Unit/Building/ExtraView.cs
+++ AgeOfWar2/Assets/Scripts/Yuxiang/Unit/Building/ExtraView.cs
@@ -1,8 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using System.Linq;
 using UnityEngine;
 
 public class ExtraView : Building
 {
-   
+    [SerializeField] int damage;
+
+    public override void effect()
+    {
+        SortedDictionary<float, Tile> targets = new SortedDictionary<float, Tile>();
+
+        //check all surrounding tiles
+        foreach (Tile curTile in tile.neighbors)
+        {
+            //if can see this tile and there is enemy unit on it
+            if (!curTile.dark.activeSelf && curTile.unit != null && curTile.unit.ownerID != ownerID)
+            {
+                //attack order depending on distance to mainbase
+                targets.TryAdd(dist(tile, curTile), curTile);
+            }
+        }
+
+        foreach (Tile curTile in tile.neighbors2)
+        {
+            //if can see this tile and there is enemy unit on it
+            if (!curTile.dark.activeSelf && curTile.unit != null && curTile.unit.ownerID != ownerID)
+            {
+                //attack order depending on distance to mainbase
+                targets.TryAdd(dist(tile, curTile), curTile);
+            }
+        }
+
+        //don't attack already dead troop
+        while (targets.Count > 0 && targets.Values.Last().unit.health <= 0)
+        {
+            targets.Remove(targets.Keys.Last());
+        }
+
+        if (targets.Count > 0)
+        {
+            targets.Values.Last().unit.PV.RPC(nameof(takeDamage), RpcTarget.AllViaServer, damage);
+        }
+    }
 }
