13c562c97153abcfc600d9a861a95c9f3815ee51
Yuxiang Huang <yhuang30@stuy.edu>
2023-03-19 14:08:03 -0400

    Forward and backward buttons done for tutorial

--- AgeOfWar2/Assets/Scripts/Yuxiang/Main/TutorialManager.cs
+++ AgeOfWar2/Assets/Scripts/Yuxiang/Main/TutorialManager.cs
@@ -7,14 +7,23 @@ using Photon.Pun;
 public class TutorialManager : MonoBehaviour
 {
     [SerializeField] Canvas tutorialCanvas;
-    [SerializeField] List<TextMeshProUGUI> instructions;
+    [SerializeField] List<GameObject> instructions;
     [SerializeField] int index;
+    [SerializeField] GameObject forwardbtn;
+    [SerializeField] GameObject backwardbtn;
 
     private void Start()
     {
         if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Tutorial") &&
             (bool)PhotonNetwork.CurrentRoom.CustomProperties["Tutorial"]){
             tutorialCanvas.gameObject.SetActive(true);
+            backwardbtn.SetActive(false);
+            forwardbtn.SetActive(true);
+            foreach(GameObject text in instructions)
+            {
+                text.SetActive(false);
+            }
+            instructions[0].SetActive(true);
         }
         else
         {
@@ -22,11 +31,38 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
-    public void changeInstruction(int dir)
+    public void forward()
     {
+        if (index == 0)
+        {
+            backwardbtn.SetActive(true);
+        }
+
         instructions[index].gameObject.SetActive(false);
-        index += dir;
+        index++;
         instructions[index].gameObject.SetActive(true);
+
+        if (index == instructions.Count - 1)
+        {
+            forwardbtn.SetActive(false);
+        }
+    }
+
+    public void backward()
+    {
+        if (index == instructions.Count - 1)
+        {
+            forwardbtn.SetActive(true);
+        }
+
+        instructions[index].gameObject.SetActive(false);
+        index--;
+        instructions[index].gameObject.SetActive(true);
+
+        if (index == 0)
+        {
+            backwardbtn.SetActive(false);
+        }
     }
 
     public void endTutorial()
